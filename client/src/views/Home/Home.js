6fac7e45ced5daa3d483b4b266443c88b6f1744b
achaltelmasre <achaltelmasre@gmail.com>
2024-03-27 18:11:47 +0530

    add navbar component

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,11 +1,13 @@
-
+import Navbar from "../../components/Navbar/Navbar.js/Navbar";
 import "./Home.css"
 
 function Home(){
    
     return(
         <>
+        <Navbar/>
           <h1>Home</h1>
+
         </>
     );
     }
