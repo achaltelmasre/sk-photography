da2fe05135007d49bbd6c0e6a2f54e08753b5dfe
achaltelmasre <achaltelmasre@gmail.com>
2024-04-10 17:46:59 +0530

    add btn and change navbar bg

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,19 +13,19 @@ import { Link } from "react-router-dom";
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse back-remo" id="navbarNav">
+    <div class="collapse navbar-collapse back-remo " id="navbarNav">
       <ul class="navbar-nav mx-lg-auto">
         <li class="nav-item">
-          <Link class="nav-link active ms-5 hover " aria-current="page" to="/" >Home</Link>
+          <Link class="nav-link active ms-5 hover text-light " aria-current="page" to="/" >Home</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link active ms-5 hover" aria-current="page" to="/about" >Photos</Link>
+          <Link class="nav-link active ms-5 hover text-light" aria-current="page" to="/about" >Photos</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link active ms-5 hover" aria-current="page" to="/order" >Reviews</Link>
+          <Link class="nav-link active ms-5 hover text-light" aria-current="page" to="/order" >Reviews</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link active ms-5 hover" aria-current="page" to="/contact" >Contact</Link>
+          <Link class="nav-link active ms-5 hover text-light" aria-current="page" to="/contact" >Contact</Link>
         </li>
       </ul>
     </div>
--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,6 +1,7 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Slider from "../../components/Slider/Slider";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -12,6 +13,8 @@ function Home() {
           
           <div className="head-div ">
           <h1 className="display-4 head-name" >SK PHOTOGRAPHY</h1>
+          <p className="text-light ps-3">I take photographs with creativity, concept and passion</p>
+          <a href="https://www.instagram.com/sk_photography7768/  " className="bg-light text-dark ps-4 pe-4 p-2 head-link" >See My Portfolio</a>
         
           </div>
           
