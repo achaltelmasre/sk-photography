ce88485c6b8190bc9dab0add6c7290991fde7f54
achaltelmasre <achaltelmasre@gmail.com>
2024-04-13 19:52:42 +0530

    update text and change slider img

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -14,10 +14,10 @@ function Home() {
         <div className="back-img">
           
           <div className="head-div ">
-            <h1 className="mb-4">Welcome....</h1>
+            <h1 className="mb-3">Welcome....</h1>
           <h1 className="display-4 head-name" >SK PHOTOGRAPHY</h1>
-          <p className="text-light ps-3">I take photographs with creativity, concept and passion</p>
-          <p className="text-light ps-3">I'm  freelance photographer specializing in people & portrait photography, based in Landon. 
+          <p className="text-light fs-5 ">I take photographs with creativity, concept and passion .</p>
+          <p className="text-light">I'm  freelance photographer specializing in people & portrait photography, based in Bhandara. 
             Photography is my passion and i love to turn ideas into beautiful things.
             Looking for a caring , fun , passionate and creative photographer? It's me!
           </p>
