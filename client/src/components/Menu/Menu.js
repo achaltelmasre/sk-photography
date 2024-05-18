e9cae3d2f306ffd0dad44a56a259749c08aa30c3
achaltelmasre <achaltelmasre@gmail.com>
2024-05-18 15:48:43 +0530

    complete menu component

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,25 +1,33 @@
 
 import "./Menu.css"
 import wedding from './img/wedding.jpg';
-import engeage from './img/enage.jpg';
+import engeage from './img/engage.jpg';
 import mehendi from './img/mehndi.jpg'
 import prewedding from './img/prewedding.jpg'
+import babyshower from './img/babyshower.jpg'
+import birthday from './img/birthday.jpg'
+import baby from './img/baby.jpg'
 import festival from './img/festival.jpg';
+
 
 const Menu  = () => {
 
     return (
         <>
-         <div className="menu-div">
-            <img src={wedding} className=" menu-img" alt="img1" />
-            <a><img src={engeage} className="menu-img" alt="img2" /></a>
-            <a><img src={prewedding} className="menu-img" alt="img3" /></a>
-            <a><img src={mehendi} className="menu-img" alt="img4" /></a>
-            <a><img src={festival} className="menu-img" alt="img5" /></a>
-            <a><img src="" className="menu-img" alt="img6" /></a>
-            <a><img src="" className="menu-img" alt="img7" /></a>
-            <a><img src="" className="menu-img" alt="img8" /></a>
-
+         <div >
+            <h1 className="text-center p-3"> PHOTO GALLERY</h1>
+            <div className="menu-div">
+            <a href="#"><img src={wedding} className=" menu-img" alt="img1" /><h5>Wedding</h5></a>
+            <a href="#"><img src={engeage} className="menu-img" alt="img2" /><h5>Engagement</h5></a>
+            <a href="#"><img src={prewedding} className="menu-img" alt="img3" /><h5>Pre-wedding</h5></a>
+            <a href="#"><img src={mehendi} className="menu-img" alt="img4" /><h5>Mehendi</h5></a>
+            </div>
+            <div className="menu-div">
+            <a href="#"><img src={babyshower} className="menu-img" alt="img5" /><h5>Babyshower</h5></a>
+            <a href="#"><img src={birthday} className="menu-img" alt="img6" /><h5>Birthday</h5></a>
+            <a href="#"><img src={baby} className="menu-img" alt="img7" /><h5>Baby-shot</h5></a>
+            <a href="#"><img src={festival} className="menu-img" alt="img8" /><h5>Festival</h5></a>
+            </div>
 
          </div>
 
