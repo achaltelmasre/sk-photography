ab2849b47f37d1dc189c11904dbcebb982e62e87
achaltelmasre <achaltelmasre@gmail.com>
2024-06-09 19:16:38 +0530

    responsive navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,13 +8,13 @@ import { Link } from "react-router-dom";
 { 
   return (
     <nav class="navbar navbar-expand-lg fs-4 nav1  ">
-  <div class="container-fluid ">
-    <a class="navbar-brand ms-2" href="#"> <img src={logo} alt="logo" height="60px"/> <span className="fs-4 text-red  mt-1"> Photography</span></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <div class="container-fluid text-light">
+    <a class="navbar-brand ms-2 text-light" href="#"> <img src={logo} alt="logo" height="60px"/> <span className="fs-4 text-red  mt-1"> Photography</span></a>
+    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon text-light"></span>
     </button>
-    <div class="collapse navbar-collapse back-remo " id="navbarNav">
-      <ul class="navbar-nav mx-lg-auto">
+    <div class="collapse navbar-collapse text-light " id="navbarNav">
+      <ul class="navbar-nav mx-lg-auto ">
         <li class="nav-item">
           <Link class="nav-link active ms-5 hover text-light " aria-current="page" to="/" >Home</Link>
         </li>
