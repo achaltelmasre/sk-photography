3104698ced4b3a267a602eaa69150b1141eb2d34
achaltelmasre <achaltelmasre@gmail.com>
2024-06-22 20:19:02 +0530

    apply css on contact page

--- client/src/views/Contact/Contact.js
+++ client/src/views/Contact/Contact.js
@@ -10,8 +10,8 @@ function Contact () {
            <div>
              <Navbar/>
                 
-                <div>
-                  <h2>Contact Us</h2>
+                <div className='contact-container text-center'>
+                <h2 className='contact-name'>Contact Us</h2>
 
                   <h4>Bhandara , 441904 </h4>
                   <p>Check out our location</p>
@@ -25,45 +25,70 @@ function Contact () {
                    <h4>Mon to Sun - 11:00 to 9:00</h4>
                    <p>Our common business hours</p>
                    <br/>
-                    <i class="bi bi-facebook"></i>
-                    <i class="bi bi-whatsapp"></i>
-                    <i class="bi bi-instagram"></i>
-                    <i class="bi bi-twitter"></i>
+                    <i class="bi bi-facebook icon"></i>
+                    <i class="bi bi-whatsapp icon"></i>
+                    <i class="bi bi-instagram icon"></i>
+                    <i class="bi bi-twitter icon"></i>
 
                 </div>
 
-                <div>
-                   <h2>Enquiry form</h2>
-                   <p>Will be GLAD to be a part of your SPECIAL DAY</p>
+                <div className='contact-container '>
+                   <h2 className='enquiry-name'>Enquiry form</h2>
+                   <p className='text-center'>Will be GLAD to be a part of your SPECIAL DAY</p>
 
-                   <label>Your Name : </label>
-                   <input placeholder='Enter your name '></input>
-                    <br/>                
-                   <lable>Phone No. : </lable>
-                   <input placeholder='Enter your number'></input>
-
-                   <label>Email : </label>
-                   <input placeholder='Enter your email'></input>
-                   <br/>
-                   <lable>Date : </lable>
-                   <input type='date'></input>
-
-                   <lable>Events : </lable>
-                   <input type='text' placeholder='Enter ypur events'></input>
-                   <br/>
-                   <lable>City/venu</lable>
-                   <input type='text' placeholder='exter your city and venu'></input>
-                   <br />
-                   <lable>Number of Guests Expected </lable>
-                   <input type='text'></input>
-                   <br/>
-                   <label>Any special requirements or anything you want to mention?
-                   </label>
-                   <input type='text' placeholder='enter your requirements '></input>
-                   <br />
-                   <lable>How Did You Find Us? </lable>
-                   <input type='text'></input>
+                  <div>
+                  <label>Your Name : </label>
+                  <input placeholder='Enter your name ' className='width'></input>
+                  </div>
                   
+                   <div className='d-flex flex-row '>
+                   <div  className='me-5'>
+                    <label>Phone No. :</label>
+                    <input placeholder='Enter your number'className='flex-width' ></input>
+                     </div>               
+                  
+                  <div className='ms-5'>
+                  <label>Email : </label>
+                  <input placeholder='Enter your email' className='flex-width'></input>
+                  </div>
+                   </div>
+
+                    
+                  
+               <div className='d-flex flex-row '>
+               <div className='  me-5'>
+                  <label>Date :</label>
+                   <input type='date'className='flex-width' ></input>
+                  </div>
+                   
+                   <div className=' ms-5'>
+                   <label>Events :</label>
+                   <input type='text' placeholder='Enter ypur events' className='flex-width'></input>
+                   </div>
+               </div>
+                
+                 <div>
+                 <label>City/venu :</label>
+                 <input type='text' placeholder='exter your city and venu' className='width'></input>
+                 </div>
+                  
+                     <div>
+                     <label>Number of Guests Expected </label>
+                     <input type='text'placeholder='expected guests' className='width'></input>
+                     </div>
+                   
+                  <div>
+                  <label>Any special requirements or anything you want to mention? </label>
+                  <input type='text' placeholder='enter your requirements ' className='width'></input>
+                  </div>
+                   
+             
+           <div>
+      <label>How Did You Find Us?</label>
+      <input type='text'placeholder='find us on' className='width'></input>
+      </div>
+                   
+                   
                    <button type="button" class="btn btn-secondary">Secondary</button>
 
                 </div>
