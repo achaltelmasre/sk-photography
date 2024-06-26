91df7db21d7afc3162500c43d70feba827fb048d
achaltelmasre <achaltelmasre@gmail.com>
2024-06-26 16:14:46 +0530

    update sendmail function

--- server/controller/events.js
+++ server/controller/events.js
@@ -1,4 +1,5 @@
 import Event from "../model/Events.js"; // Ensure the correct path and file extension
+import EnquiryMail from "./sendMail.js";
 
 const postApiEvents = async (req, res) => {
     const { name, phone_number, email, date, events, city_venue, expected_guests, special_requirements, how_did_you_find_us } = req.body;
@@ -17,6 +18,7 @@ const postApiEvents = async (req, res) => {
   
     try {
       const savedEvent = await newEvent.save();
+      await EnquiryMail(savedEvent);
       res.status(201).json(savedEvent);
     } catch (error) {
       res.status(400).json({ message: error.message });
--- server/controller/sendMail.js
+++ server/controller/sendMail.js
@@ -1,9 +1,12 @@
+// sendVerifyMail.js
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sendVerifyMail = async (name, email, tempUserId) => {
+const EnquiryMail = async (eventData) => {
+    const { name, email, phone_number, date, events, city_venue, expected_guests, special_requirements, how_did_you_find_us } = eventData;
+
     try {
         const transporter = nodemailer.createTransport({
             host: 'smtp.gmail.com',
@@ -11,18 +14,28 @@ const sendVerifyMail = async (name, email, tempUserId) => {
             port: 465,
             secure: true,
             auth: {
-                user: 'achaltelmasre@gmail.com',
+                user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
             }
         });
 
         const mailOptions = {
             from: email,
-            to:  process.env.EMAIL_USER,
-            subject: 'Verification Mail',
+            to: 'shubhamkhamle@gmail.com',
+            subject: 'New Event Enquiry',
             html: `
-                <p>Hi!<br/> Can you allow <u><b>${name}</b></u> (${email}) to register their data?</p>
-                
+                <p>Hi!<br/>A new event enquiry  with the following details: <br/> Please contact us....</p>
+                <ul>
+                    <li><b>Name:</b> ${name}</li>
+                    <li><b>Email:</b> ${email}</li>
+                    <li><b>Phone Number:</b> ${phone_number}</li>
+                    <li><b>Date:</b> ${date}</li>
+                    <li><b>Event:</b> ${events}</li>
+                    <li><b>City/Venue:</b> ${city_venue}</li>
+                    <li><b>Number of Guests Expected:</b> ${expected_guests}</li>
+                    <li><b>Special Requirements:</b> ${special_requirements}</li>
+                    <li><b>How Did You Find Us:</b> ${how_did_you_find_us}</li>
+                </ul>
             `
         };
 
@@ -35,4 +48,4 @@ const sendVerifyMail = async (name, email, tempUserId) => {
     }
 };
 
-export default sendVerifyMail;
+export default EnquiryMail ;
