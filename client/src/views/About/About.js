7223598643d208aa5ecba97ec3bae242ba622a59
achaltelmasre <achaltelmasre@gmail.com>
2024-07-12 15:36:32 +0530

    responsive about page

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -15,18 +15,21 @@ function About () {
 
               <div class="container text-center">
   <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-6 ">
      
+     <div className='m-3 p-3'>
      <h4 className='about-text1'>SK PHOTOGRAPHY</h4>
 
-     <p>At SK Photography, based in the city of Bhandara in Maharashtra, we capture life’s most precious moments with creative flair. Specializing in cinematic wedding films, candid photography, and pre-wedding shoots, we transform your memories into timeless works of art.</p>
+<p>At SK Photography, based in the city of Bhandara in Maharashtra, we capture life’s most precious moments with creative flair. Specializing in cinematic wedding films, candid photography, and pre-wedding shoots, we transform your memories into timeless works of art.</p>
 
-     <p>Our dedication to excellence has not gone unnoticed. We are proud to have received the Best Photography award, a testimony to our commitment to quality and innovation. With ten years of solid experience, our team of top photographers, talented production professionals, and skilled touch-up artists work tirelessly to bring your imagination to life.</p>
+<p>Our dedication to excellence has not gone unnoticed. We are proud to have received the Best Photography award, a testimony to our commitment to quality and innovation. With ten years of solid experience, our team of top photographers, talented production professionals, and skilled touch-up artists work tirelessly to bring your imagination to life.</p>
 
-     <p>At SK Photography, every shot is a blend of passion and professionalism, ensuring that your special moments are captured beautifully and authentically. Join us and experience the magic of SK Photography – where every picture tells a story.</p>
+<p>At SK Photography, every shot is a blend of passion and professionalism, ensuring that your special moments are captured beautifully and authentically. Join us and experience the magic of SK Photography – where every picture tells a story.</p>
+     </div>
+    
       
     </div>
-    <div class="col-md-6">
+    <div class="col-md-6 ">
       
      <h4 className='about-text2'>PRINCIPLES OF OUR WORK </h4>
 
